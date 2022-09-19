beb6ccae0da4ca5f794bdd6155ca47d992bca6e4
Isaigarcia27 <113812566+Isaigarcia27@users.noreply.github.com>
2022-09-18 21:09:27 -0700

    updates to my lessons

--- fuckaroundlists.py
+++ fuckaroundlists.py
@@ -6,4 +6,10 @@ Created on Sun Sep 18 20:14:09 2022
 @author: isaigarcia
 """
 
-test
+def for_loop_program():
+    for num in [0,1,2,3,4]:
+        print(num)
+
+def print_till_num(this):
+    for num in range(this):
+        print(num)
---
+++ math_functions.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 18 20:37:14 2022
+
+@author: isaigarcia
+"""
+# HW for Isai:
+#   The output of 'f' should be:
+#       "x gives us the value f(x)"
+#   Example with x = 3 (y = 8)
+#       "3 gives us the value 8"    
+def f(x):
+    return 3*x-1
+
+def g1(x):
+    return x**2 - 7*x + 12
+
+def g2(x):
+    return (x - 3)*(x - 4)
